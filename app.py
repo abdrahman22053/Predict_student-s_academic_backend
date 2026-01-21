36357d8c78cc406ebf0aeece971396afd2890f14
abdrahman22053 <abderahmanhaimeda@gmail.com>
2026-01-21 03:28:13 +0000

    docs: Update README with deployment info and custom favicon

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://student-ml-frontend.vercel.app/",  
+        "https://student-ml-frontend.vercel.app",  
     ],
     allow_credentials=True,
     allow_methods=["*"],
